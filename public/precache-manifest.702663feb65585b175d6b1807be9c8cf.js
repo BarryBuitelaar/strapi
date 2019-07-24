41bab6e68f657cda17a202004190c31313e5cfb3
Barry Buitelaar <barry.buitelaar@inqdo.com>
2019-07-24 16:16:50 +0200

    no message

---
+++ public/precache-manifest.702663feb65585b175d6b1807be9c8cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f38b576d032be4cadb5c",
+    "url": "/static/js/main.28647029.chunk.js"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f38b576d032be4cadb5c",
+    "url": "/static/css/main.d6367302.chunk.css"
+  },
+  {
+    "revision": "f68f7c15ba8fd3d150b5445758a29e4c",
+    "url": "/index.html"
+  }
+];
